c3b1e5817d7e82b7c0b4293e95db69c68634edb0
matfire <mgassend@gmail.com>
2024-01-16 10:49:11 +0000

    add: icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import cloudflare from "@astrojs/cloudflare";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
   adapter: cloudflare()
 });
